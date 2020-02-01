7812f3303278f1dc17158d4703ab321eda188614
Cameron Gorrie <cameron.gorrie@tophatmonocle.com>
2020-02-01 00:07:16 -0500

    get rid of all email stuff

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-import sendWelcomeEmail from './send-welcome-email';
-
-if (!process.env.MAILJET_KEY || !process.env.MAILJET_SECRET || !process.env.SENDER_EMAIL) {
-  console.error('Please prove mailjet details in your .env file (see .env.example).');
-  process.exit(1);
-}
-
 const app = express();
 
 app.use(
@@ -28,10 +21,6 @@ app.post('/', async (req, res) => {
   console.log(JSON.stringify(req.body, null, 2));
   console.log('****************');
 
-  const { email } = req.body.event.data.new;
-  console.log('got email', email);
-
-  await sendWelcomeEmail(email);
   res.status(200).json({ status: 'OK' });
 });
 
